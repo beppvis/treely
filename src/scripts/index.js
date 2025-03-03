2feb566773f414722b1f207ec07b006f39ad0f23
beppvis <visruthvijind@gmail.com>
2025-03-03 20:32:20 +0530

    made some animation changes with completing the index

---
+++ src/scripts/index.js
@@ -0,0 +1,33 @@
+
+function scrollDown() {
+    let id = null;
+    const mainPanel = document.getElementById("mainPanel");
+    const mainSlidePanel = document.getElementById("mainSlidePanel");
+    console.log("bruh");
+
+    //let pos = -900;
+    clearInterval(id);
+
+
+    //mainPanel.style.display = 'none';
+    //mainSlidePanel.style.display = 'flex';
+    //id = setInterval(frame, 15);
+    //function frame() {
+    //    if (pos >= 0) {
+    //        clearInterval(id);
+    //    }
+    //    else {
+    //        pos += 10;
+    //        mainSlidePanel.style.bottom = pos + 'px';
+    //    }
+    //}
+
+    mainPanel.style.display = 'none';
+    mainSlidePanel.style.display = 'flex';
+    mainSlidePanel.style.animation = "magicAppear 0.3s ease-out";
+
+
+
+
+
+}
