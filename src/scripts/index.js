803dc84f6fab1bf653091396f8aeca6a86fe359c
beppvis <visruthvijind@gmail.com>
2025-03-21 19:09:14 +0530

    try the about Us feature might work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,11 +10,22 @@ var tress = new Array()
 
 
 function onLoad() {
-    console.log("hello")
+    const visibleCaroselItems = document.getElementsByClassName("visibleCarouselItem");
+}
+
+
+function switchToMe(item) {
+    let parent = item.parentNode;
+    let parentClassName = item.parentNode.className;
+    if (!parentClassName.includes("visibleCarouselItem ")) {
+        this.style = "animation:switchIn 0.5s;";
+        console.log(parent.style)
+    }
+
 }
 
 function spawnCarouselTab(tree) {
-    var parent = document.getElementById("carosel");
+    var parent = document.getElementById("carosel").getRootNode().getAttributes();
     var carouselBody = document.createElement("div");
     carouselBody.className = "displayBar";
     var image = tree.image.cloneNode();
