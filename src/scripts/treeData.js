fa9c6b053d6f54610d0128f95d3084d5983701ba
beppvis <visruthvijind@gmail.com>
2025-03-25 21:38:00 +0530

    added treeData, need to add hooks

---
+++ src/scripts/treeData.js
@@ -0,0 +1,29 @@
+
+
+function drawTrees(slider) {
+    var noTrees = slider.value;
+    var treeImg = document.getElementById("forrestTreeImage");
+    var treeCount = document.getElementById("treeCount");
+    treeCount.innerText = slider.value;
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < noTrees; i++) {
+        ctx.beginPath();
+        if (i == 0) {
+            ctx.drawImage(treeImg, (canvas.width / 2 - 60), canvas.height - 120, 60, 120)
+        }
+        else if (i % 2 == 0) {
+            var height = Math.floor(Math.random() * 30 + 100)
+            ctx.drawImage(treeImg, (canvas.width / 2 - 55) - i * 20, canvas.height - height, 60, height)
+        }
+        else {
+            var height = Math.floor(Math.random() * 20 + 100)
+            ctx.drawImage(treeImg, (canvas.width / 2 - 55) + i * 20, canvas.height - height, 60, height)
+        }
+
+        ctx.closePath();
+    }
+
+}
+
