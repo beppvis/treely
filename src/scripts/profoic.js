82fa219617f7a244d80cde6dff3f30e2741f9372
parthivsurya <parthivsuryakb@gmail.com>
2025-04-09 00:20:48 +0530

    marginleft updatede

---
+++ src/scripts/profoic.js
@@ -0,0 +1,11 @@
+function loggedIn() {
+    if (localStorage.getItem("activeUser") != null)
+        document.getElementById("profilePic").classList.add("active")
+    else
+        return false
+}
+
+ function onLoad() {
+
+    loggedIn()
+}
